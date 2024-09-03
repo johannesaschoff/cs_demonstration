f420288ac2cc4c3b2bbe6a1c47404be07aff6daa
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-09-03 15:53:58 +0200

    Delete test_font.py

--- test_font.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Load the custom CSS from style.css
-with open("style.css") as css_file:
-    st.markdown(f'<style>{css_file.read()}</style>', unsafe_allow_html=True)
-
-# Your Streamlit app content
-st.title("Custom Font Test")
-st.write("This text should be rendered in the custom font 'UnitSlabOT-Bold'.")
