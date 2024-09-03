f0f26b86a5080db2f1fc9a799cbbb34cff13670b
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-09-03 15:54:27 +0200

    Delete font_working.py

--- font_working.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Load HTML content
-with open('us_sectors.html', 'r') as file:
-    html_content = file.read()
-
-# Display HTML content in Streamlit
-st.markdown(html_content, unsafe_allow_html=True)
