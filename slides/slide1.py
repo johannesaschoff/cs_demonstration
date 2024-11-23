745f8a41a95f574c6f61701b846037d8f9f7d066
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-11-23 18:05:54 +0100

    Update slide1.py

--- slides/slide1.py
+++ slides/slide1.py
@@ -12,7 +12,7 @@ def render():
     st.write("- Sets the stage for deeper dives into data.")
     display_slideshow(
         images=[
-            "https://github.com/johannesaschoff/cs_demonstration/blob/main/slides/image1.jpg",
+            "https://github.com/johannesaschoff/cs_demonstration/blob/0d3cd630c31e95ad3bc61d0b98c7ceb7d710b53b/slides/image1.jpg",
             "https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920",
             "https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920"
         ],
