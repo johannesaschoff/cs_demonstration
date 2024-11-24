77ce31057ed15f43e9d89c11880ac1ce37d393f2
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-11-24 16:58:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import streamlit as st
-from PIL import Image
-import requests
-from io import BytesIO
-import time
 
-# List of GitHub raw image URLs
-image_urls = [
-    "https://github.com/johannesaschoff/cs_demonstration/main/images/image1.jpg",
-    "https://github.com/johannesaschoff/cs_demonstration/main/images/image1.jpg",
-    "https://github.com/johannesaschoff/cs_demonstration/main/images/image1.jpg",
-]
+# GitHub image URL
+image_url = "https://github.com/johannesaschoff/cs_demonstration/main/images/image1.jpg"
 
-st.title("Image Slideshow from GitHub")
+# Streamlit app
+st.title("Display Image from GitHub")
+st.image(image_url, caption="Image from GitHub", use_column_width=True)
 
-# Option for slideshow speed
-slideshow_speed = st.slider("Select slideshow speed (seconds):", 1, 10, 3)
-
-# Function to load an image from a URL
-def load_image(url):
-    response = requests.get(url)
-    return Image.open(BytesIO(response.content))
-
-# Show slideshow
-if st.button("Start Slideshow"):
-    for url in image_urls:
-        img = load_image(url)
-        st.image(img, use_column_width=True)
-        time.sleep(slideshow_speed)
+st.write("This image is hosted on GitHub. Make sure the URL points to the raw version of the image.")
