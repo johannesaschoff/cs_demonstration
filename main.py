df89514f79d4202178bb6d06bd8f9054e3a8003f
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-11-24 18:23:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from slides import slide1, slide2, slide3, slide4, slide5
+from slides import slide1, slide2, slide3, slide4, slide5, slide6
 
 # Add a logo to the top of the sidebar
 logo_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/logo_grey.png" 
@@ -27,3 +27,5 @@ elif selected_slide == slides[3]:
     slide4.render()
 elif selected_slide == slides[4]:
     slide5.render()
+elif selected_slide == slides[5]:
+    slide6.render()
