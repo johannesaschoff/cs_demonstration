a51ab4eb2cdfb2baaf679e95c5250c8232564aa0
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-11-24 18:15:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import streamlit as st
 from slides import slide1, slide2, slide3, slide4, slide5
 
+# Add a logo to the top of the sidebar
+logo_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/logo_white.png" 
+st.sidebar.image(logo_url, use_column_width=True)
+
 # Sidebar for slide selection
 st.sidebar.title("Navigation")
-slides = ["Craftsmanship and production", 
-          "Educational Development", 
-          "Community Development and Employment", 
-          "Emergency Relief and Basic Needs", 
-          "Food Security and Sustainable Agriculture"]
+slides = [
+    "Craftsmanship and production",
+    "Educational Development",
+    "Community Development and Employment",
+    "Emergency Relief and Basic Needs",
+    "Food Security and Sustainable Agriculture",
+]
 selected_slide = st.sidebar.radio("Choose a slide", slides)
 
 # Render the selected slide
