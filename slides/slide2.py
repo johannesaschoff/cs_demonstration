8f78ebaa72aadc3a4805f779aa98608a649fb18e
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-05 19:17:52 +0100

    Update slide2.py

--- slides/slide2.py
+++ slides/slide2.py
@@ -105,7 +105,7 @@ def render():
             st.download_button(
                 label="Download data as Excel",
                 data=excel_data,
-                file_name="corporate_education.xlsx",
+                file_name="Educational_Development.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             )
         else:
