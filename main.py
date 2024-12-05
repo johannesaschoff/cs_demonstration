9f1a580ae20d6b36a2c3f89c9d2a0a4cd3128ef3
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-05 18:54:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from slides import slide1, slide2, slide3, slide4, slide5
 # Add a logo to the top of the sidebar
 logo_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/logo_grey.png" 
