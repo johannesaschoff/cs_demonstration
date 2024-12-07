36a51c7fb6176e4c88f4265190c72cf9a88922db
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-07 23:34:55 +0100

    Update slide1.py

--- slides/slide1.py
+++ slides/slide1.py
@@ -26,7 +26,7 @@ def render():
 
     # Section: Slideshow
     st.markdown("**Pitchdeck Preview**")
-    columns = st.columns(6)
+    columns = st.columns(5)
 
     with columns[0]:
         st.image(
@@ -78,15 +78,6 @@ def render():
             use_column_width=True
         )
 
-    with columns[5]:
-        st.image(
-            "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/image6.png",
-            use_column_width=True
-        )
-        st.image(
-            "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/image12.png",
-            use_column_width=True
-        )
     
     pptx_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/Pitch.pptx"
     try:
