31a5a33e9d0a33fb2e58b5a051cc7c7a22693391
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-12 11:37:52 +0100

    Update slide2.py

--- slides/slide2.py
+++ slides/slide2.py
@@ -100,6 +100,8 @@ def render():
         # Load the dataset
         df = pd.read_csv(csv_url)
         df = df[df["Educational Development"] == True]
+        df = df.rename(columns={"Contact Mail": "Contact Mail/Phone Nr./LinkedIn"})
+
 
         df["Industries"] = df["Industries"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
 
