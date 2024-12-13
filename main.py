ebf8c962e6b5196543bd53923b5aee8088557e4a
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-13 21:47:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from slides import slide1, slide2, slide3, slide4, slide5, slideKPI, KPIdashboard
+from slides import slide1, slide2, slide3, slide4, slide5, slideKPI, dashboardKPI
 
 st.set_page_config(layout="wide") 
 
@@ -33,5 +33,5 @@ elif selected_slide == slides[4]:
     slide5.render()
 elif selected_slide == slides[5]:
     slideKPI.render()
-elif selected_slide == slides[5]:
-    KPIdashboard.render()
+elif selected_slide == slides[6]:
+    dashboardKPI.render()
