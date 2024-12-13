c71737dffe0faeaf1b48a25eda464ec15b17e5a3
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-13 14:51:38 +0100

    Update slideKPI.py

--- slides/slideKPI.py
+++ slides/slideKPI.py
@@ -1,68 +1,13 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import requests
-import ast
 
-# Set page configuration - this must be the first Streamlit command
-st.set_page_config(layout="wide")
+st.title("Edit Vendor Areas in Google Sheets")
 
-# Fetch data directly from Google Sheets
+# Fetch data directly without caching
 def fetch_data():
     conn = st.connection("gsheets", type=GSheetsConnection)
     return conn.read(worksheet="Names")
 
-# Fetch PPTX file from a URL
-def fetch_pptx(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        raise Exception(f"Failed to fetch the PPTX. Status code: {response.status_code}")
 existing_data = fetch_data()
 st.dataframe(existing_data)
-# Render the app UI
-def render():
-    st.title("KPI")
-    st.markdown("**Project types**")
-    st.write("- Butchery")
-    st.write("- Bakery")
-    st.write("- Kitchen")
-    st.write("- Woodwork")
-    st.write("- Sewing")
-    st.write("- Metal Construction Workshop")
-
-    # Section: Slideshow
-    st.markdown("**Pitchdeck Preview**")
-    columns = st.columns(5)
-
-    image_urls = [
-        ["image_1.4.png", "image_6.png"],
-        ["image_2.png", "image_7.png"],
-        ["image_3.png", "image_8.png"],
-        ["image_4.png", "image_9.png"],
-        ["image_5.4.png", "image_10.png"]
-    ]
-
-    for col, urls in zip(columns, image_urls):
-        for url in urls:
-            col.image(
-                f"https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/images/{url}",
-                use_container_width=True
-            )
-
-    pptx_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/PitchDeck production.pptx"
-    try:
-        pptx_data = fetch_pptx(pptx_url)
-        st.download_button(
-            label="Download PPTX File",
-            data=pptx_data,
-            file_name="PitchDeck.pptx",
-            mime="application/vnd.openxmlformats-officedocument.presentationml.presentation",
-        )
-    except Exception as e:
-        st.error(f"Could not fetch the PPTX file: {e}")
-
-
-
-render()
