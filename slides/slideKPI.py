ebfda30e4832d391fefba62b9274cf31e1c0ff66
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-13 14:52:34 +0100

    Update slideKPI.py

--- slides/slideKPI.py
+++ slides/slideKPI.py
@@ -2,12 +2,14 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-st.title("Edit Vendor Areas in Google Sheets")
-
-# Fetch data directly without caching
-def fetch_data():
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    return conn.read(worksheet="Names")
-
-existing_data = fetch_data()
-st.dataframe(existing_data)
+def render():
+    st.title("Edit Vendor Areas in Google Sheets")
+    
+    # Fetch data directly without caching
+    def fetch_data():
+        conn = st.connection("gsheets", type=GSheetsConnection)
+        return conn.read(worksheet="Names")
+    
+    existing_data = fetch_data()
+    st.dataframe(existing_data)
+render()
