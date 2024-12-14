5082698495c038b286331b5b705f92d526f65f75
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-14 01:40:41 +0100

    Update dashboardKPI.py

--- slides/dashboardKPI.py
+++ slides/dashboardKPI.py
@@ -24,6 +24,11 @@ def fetch_google_sheets_data(sheet_id, range_name):
 def render():
     st.title("KPI Dashboard")
 
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
+
     # Google Sheets details
     sheet_id = "1TPZ-lKKTrLK3TcG2r7ybl24Hy2SWLC2rhinpNRmjewY"  # Replace with your Google Sheet ID
     range_name = "KPIdashboard"  # Replace with your sheet's range (e.g., 'Sheet1!A1:D10')
