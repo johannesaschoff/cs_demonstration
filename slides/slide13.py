938f303f0d895ef5ce365e3a9e631d7afa2bc8f6
johannesaschoff <148279770+johannesaschoff@users.noreply.github.com>
2024-12-14 15:31:24 +0100

    Update slide13.py

--- slides/slide13.py
+++ slides/slide13.py
@@ -73,7 +73,7 @@ def render():
                 use_container_width=True
             )
 
-    pptx_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/PitchDeck education.pptx"
+    pptx_url = "https://raw.githubusercontent.com/johannesaschoff/cs_demonstration/main/PitchDeck community development.pptx"
     try:
         pptx_data = fetch_pptx(pptx_url)
         st.download_button(
